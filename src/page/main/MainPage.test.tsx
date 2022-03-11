3f0a59d0cc3ffdf32ab4ba313aa62f1f44959227
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2022-03-11 13:18:13 +0100

    Removed settings button

--- src/page/main/MainPage.test.tsx
+++ src/page/main/MainPage.test.tsx
@@ -16,6 +16,7 @@ describe("main page", () => {
 
 		expect(screen.queryByTestId("loading-indicator")).toBeInTheDocument()
 	})
+
 	it("should show data when it has arrived.", () => {
 		useFetchSpy.mockImplementation(() => pokemon1)
 		localStorage.clear()
@@ -24,6 +25,7 @@ describe("main page", () => {
 		expect(screen.queryByTestId("loading-indicator")).not.toBeInTheDocument()
 		expect(screen.queryByTestId("pokemon-image")).toBeVisible()
 	})
+
 	it("should lose after four incorrect guesses", () => {
 		useFetchSpy.mockImplementation(() => pokemon1)
 		localStorage.clear()
@@ -50,6 +52,7 @@ describe("main page", () => {
 			"true",
 		)
 	})
+
 	it("should add pokemon to pokedex after correct guess", () => {
 		useFetchSpy.mockImplementation(() => pokemon1)
 		localStorage.clear()
